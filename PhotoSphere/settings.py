ad2416026873c2194b315991260f6d02e9b5a368
souhardya Mallik <mail.sivsankarmallik@gmail.com>
2026-04-22 23:52:38 +0530

    Switch database engine from SQLite to PostgreSQL

--- PhotoSphere/settings.py
+++ PhotoSphere/settings.py
@@ -84,8 +84,14 @@ WSGI_APPLICATION = 'PhotoSphere.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photospheredb',
+        'USER': 'souhardya',
+        'PASSWORD': 'Sou1361122@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
